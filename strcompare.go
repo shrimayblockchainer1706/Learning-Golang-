6ae17312206b911516080c033c14071cf99958bb
shrimayblockchainer1706 <shrimayblockchainer0617@gmail.com>
2026-03-02 09:52:13 +0530

    Add files via upload

---
+++ strcompare.go
@@ -0,0 +1,16 @@
+// write go a program to compare two strings
+
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "Shrimay"
+	var b string = "Shrimay"
+
+	if a == b {
+		fmt.Println("same string")
+	} else {
+		fmt.Println("different string")
+	}
+}
