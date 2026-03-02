a2afe0d3f23efd5a4530c00ee991a73f41017aee
shrimayblockchainer1706 <shrimayblockchainer0617@gmail.com>
2026-03-02 09:56:07 +0530

    Add files via upload

---
+++ strlength.go
@@ -0,0 +1,10 @@
+// write a go program to find the length of a string
+
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "Shrimay"
+	fmt.Println("Length of the string is:", len(a))
+}
