ad72e69cab20f8a81c6156b5155a7bf066f971d7
shrimayblockchainer1706 <shrimayblockchainer0617@gmail.com>
2026-03-02 10:00:21 +0530

    Add files via upload

---
+++ strconcatenate.go
@@ -0,0 +1,12 @@
+// write a go program to concatenate two strings
+
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "Shrimay"
+	var b string = "Shrimay"
+	c := a + b
+	fmt.Println("Concatenated string is:", c)
+}
